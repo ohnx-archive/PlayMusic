ad098bd89334304ba6f1c123024c9fd3b52e540d
Mason X <masonx.01@gmail.com>
2015-09-03 21:03:15 -0400

    Close #3 and #4
    Pretty cool jam-packed update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,6 +22,7 @@ using System.Windows.Controls.Primitives;
 using Microsoft.Win32;
 using System.Diagnostics;
 
+
 namespace PlayMusic
 {
     public class Song
@@ -29,6 +30,7 @@ namespace PlayMusic
         public string Title { get; set; }
         public string Artist { get; set; }
         public string Album { get; set; }
+        public string Genre { get; set; }
         private string Filename { get; set; }
         public string getFilename()
         {
@@ -41,36 +43,39 @@ namespace PlayMusic
     }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// With the amount of try-catches in this, I doubt it'll ever crash :D
+    /// Whenever the app crashes, I just add another try-catch.
+    /// try-catches are a programmer's best friend! :)
     /// </summary>
     public partial class MainWindow : Window
     {
         private MediaPlayer mediaPlayer = new MediaPlayer();
         public List<Song> songs;
+        protected List<Song> editedSongs = new List<Song>();
         private Random rand;
         private bool isPaused = false;
         private bool isMin = false;
         private bool shuffle = true;
         private string searchdir;
+        private string prevSearchDir;
+        private string defaultsearchdir;
         KeyboardListener KListener = new KeyboardListener();
 
         public MainWindow()
         {
             InitializeComponent();
             //load search dir
-            searchdir = System.Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            searchdir = getSearchDir();
             //load songs and set in listbox
             rand = new Random();
-            songList.IsReadOnly = true;
-            genCol();
-            if (songs.Count == 0)
-            {
-                MessageBox.Show("You have no music to play!\nPlease put some music in " + searchdir + " and then run this application.\nPlease note, the file must be in .mp3 form", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                Environment.Exit(0);
-            }
+            songList.IsReadOnly = false;
+            songList.CanUserResizeRows = false;
+            songList.CanUserAddRows = false;
             //add media handler
             mediaPlayer.MediaEnded += onSongFinished;
-            //advance song
-            advanceSong();
+            songList.CellEditEnding += songList_EditedCell;
+            // gen cols and advance song
+            genCol();
             //start timer
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(0.5);
@@ -79,29 +84,78 @@ namespace PlayMusic
             //Register hotkey
             KListener.KeyDown += new RawKeyEventHandler(KListener_KeyDown);
         }
+        string getSearchDir()
+        {
+            // meant as portable friendly app
+            // string configLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)+"\\config.txt";
+            if (!File.Exists("config.txt"))
+            {
+                defaultsearchdir = System.Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            }
+            else
+            {
+                // read the place from the file
+                defaultsearchdir = File.ReadAllLines("config.txt")[0];
+            }
+            return defaultsearchdir;
+        }
         private void genCol()
         {
             songs = new List<Song>();
             TagLib.File temp;
             foreach (string s in Directory.GetFiles(searchdir, "*.mp3"))
             {
-                temp = TagLib.File.Create(s);
-                Song song = new Song { Artist = string.Join(",", temp.Tag.Performers), Title = temp.Tag.Title, Album = temp.Tag.Album };
-                song.setFilename(System.IO.Path.GetFileName(s));
-                songs.Add(song);
+                try
+                {
+                    temp = TagLib.File.Create(s);
+                    Song song;
+                    song = new Song { Artist = (temp.Tag.Performers.Length == 0 ? "Unknown" : string.Join(";", temp.Tag.Performers)),
+                                      Title = (temp.Tag.Title == null ? System.IO.Path.GetFileNameWithoutExtension(s) : temp.Tag.Title),
+                                      Album = (temp.Tag.Album),
+                                      Genre = (string.Join(";", temp.Tag.Genres)) };
+                    song.setFilename(System.IO.Path.GetFileName(s));
+                    songs.Add(song);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("There was an error reading your music: " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+            }
+            if (songs.Count == 0)
+            {
+                MessageBox.Show("You have no music to play!\nPlease put some music in " + searchdir + " and then run this application.\nPlease note, the files must be in .mp3 form", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                if (searchdir == defaultsearchdir)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    searchdir = prevSearchDir;
+                    return;
+                }
             }
             songList.ItemsSource = songs;
+            advanceSong();
+        }
+        private void refreshMusic(object whatever1, object whatever2)
+        {
+            // when running new list, I wonder if memory gets leaked... (both editedsongs and songs)
+            editedSongs = new List<Song>();
+            genCol();
         }
         private void btnShuffle_Click(object sender, RoutedEventArgs e)
         {
             if (shuffle)
             {
                 btnShuffle.Foreground = new SolidColorBrush(Color.FromRgb(51, 51, 51));
+                MenuShuffle.IsChecked = false;
                 shuffle = false;
             }
             else
             {
                 btnShuffle.Foreground = new SolidColorBrush(Color.FromRgb(68, 187, 255));
+                MenuShuffle.IsChecked = true;
                 shuffle = true;
             }
         }
@@ -131,7 +185,7 @@ namespace PlayMusic
         }
         private void playSong(Song song)
         {
-            Console.WriteLine(searchdir + "\\" + song.getFilename());
+            //Console.WriteLine(searchdir + "\\" + song.getFilename());
             mediaPlayer.Open(new System.Uri(searchdir + "\\" + song.getFilename()));
             ArtistName.Content = song.Artist;
             SongName.Content = song.Title;
@@ -149,9 +203,10 @@ namespace PlayMusic
                 toggleSong.Content = "";
                 MainWin.MaxHeight = int.MaxValue; //Because infinity doesn't work.
                 MainWin.Height = 730;
-                MainWin.MinHeight = 730;
+                MainWin.MinHeight = 300;
                 sBox.Visibility = Visibility.Visible;
                 isMin = false;
+                CMenu.Visibility = System.Windows.Visibility.Visible;
             }
             else
             {
@@ -161,6 +216,7 @@ namespace PlayMusic
                 MainWin.Height = 240;
                 MainWin.MaxHeight = 240;
                 isMin = true;
+                CMenu.Visibility = System.Windows.Visibility.Collapsed;
             }
         }
         private void btnPlayPause_Click(object sender, RoutedEventArgs e)
@@ -174,12 +230,14 @@ namespace PlayMusic
                 mediaPlayer.Play();
                 isPaused = false;
                 btnPlayPause.Content = ""; //Pause icon
+                MenuPP.Header = "Pause";
             }
             else
             {
                 mediaPlayer.Pause();
                 isPaused = true;
                 btnPlayPause.Content = ""; //Play icon
+                MenuPP.Header = "Play";
             }
         }
         void timer_Tick(object sender, EventArgs e)
@@ -191,9 +249,9 @@ namespace PlayMusic
                 else
                     lblStatus.Content = "00:00 / 00:00";
             }
-            catch (Exception erreur) //Error? Just wait for the next timer tick.
+            catch (Exception erreur) // Error? Just wait for the next timer tick.
             {
-
+                
             }
         }
 
@@ -202,7 +260,7 @@ namespace PlayMusic
             advanceSong();
         }
 
-        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void songList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             try
             {
@@ -215,7 +273,64 @@ namespace PlayMusic
             
         }
 
-        // I wonder if antivirus programs will the catch the fact that I have a system-wide keylogger bundled in my app.
+        private void songList_EditedCell(object sender, System.EventArgs e)
+        {
+            if (editedSongs.Contains((Song)songList.SelectedItem))
+            {
+
+            }
+            else
+            {
+                editedSongs.Add((Song)songList.SelectedItem);
+            }
+        }
+
+        private void saveEdits_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Save changes to ID3 tags?\nWarning: If you have any tags other than Title, Artist, Album and Genre, they will be removed.", "Save", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                TagLib.File temp;
+                List<Song> remove = new List<Song>();
+                foreach (Song s in editedSongs)
+                {
+                    try
+                    {
+                        temp = TagLib.File.Create(searchdir + "\\" + s.getFilename());
+                        temp.Tag.Performers = s.Artist.Split(';');
+                        temp.Tag.Title = s.Title;
+                        temp.Tag.Album = s.Album;
+                        temp.Tag.Genres = s.Genre.Split(';');
+                        temp.Save();
+                    }
+                    catch (IOException err)
+                    {
+                        MessageBox.Show("There was an error while saving the file: " + err.Message, "Save Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+                foreach (Song s in remove)
+                {
+                    editedSongs.Remove(s);
+                }
+                remove.RemoveRange(0, remove.Count);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void MusLocation_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog diag = new System.Windows.Forms.FolderBrowserDialog();
+            diag.SelectedPath = searchdir;
+            diag.Description = "Please select the folder to scan for music in:";
+            diag.ShowDialog();
+            prevSearchDir = searchdir;
+            searchdir = diag.SelectedPath;
+            refreshMusic(null, null);
+        }
+
+        // I wonder if antivirus programs will the catch the fact that technically, I have a system-wide keylogger bundled in my app.
         // I hope not...
         private void KListener_KeyDown(object sender, RawKeyEventArgs args)
         {
@@ -234,6 +349,11 @@ namespace PlayMusic
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             KListener.Dispose();
+        }
+
+        private void close(object sender, object something)
+        {
+            this.Close();
         }
     }
 }
